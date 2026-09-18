c0114750a2aa7c1d672e09c44c0b9e58201c86e8
cynnuh <dnlap099@gmail.com>
2026-09-18 23:49:24 +0700

    Loading

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+
+    [SerializeField]
+    private float waitSeconds = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitSeconds > 0f)
+        {
+            waitSeconds -= Time.deltaTime;
+        }
+        else
+        {
+            StartCoroutine(LoadNewScene());
+        }
+    }
+
+    private IEnumerator LoadNewScene()
+    {
+       AsyncOperation oper = SceneManager.LoadSceneAsync("BasketBall");
+       while(!oper.isDone)
+        {
+            slider.value = oper.progress / 0.9f;
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartNewGame()
     {
-        SceneManager.LoadScene("BasketBall");
+        SceneManager.LoadScene("Loading");
     }
 
     public void ExitGame()
